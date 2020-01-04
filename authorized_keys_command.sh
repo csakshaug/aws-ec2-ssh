c143a590ff464752048539e33a1896429a989cfb
Christian Sakshaug <christian@dx.no>
2020-01-04 14:07:01 +0000

    Added more logging to authorized_keys_command.sh and check if local user authorized_keys exists

--- authorized_keys_command.sh
+++ authorized_keys_command.sh
@@ -1,7 +1,30 @@
 #!/bin/bash -e
-
 if [ -z "$1" ]; then
   exit 1
+fi
+
+function log {
+	echo "$*" | logger -t list-ssh-public-keys
+}
+
+UnsaveUserName="$1"
+UnsaveUserName=${UnsaveUserName//".plus."/"+"}
+UnsaveUserName=${UnsaveUserName//".equal."/"="}
+UnsaveUserName=${UnsaveUserName//".comma."/","}
+UnsaveUserName=${UnsaveUserName//".at."/"@"}
+
+ssh_auth_keys=/home/$UnsaveUserName/.ssh/authorized_keys
+ssh_auth_keys_skip=$2
+if [ "$ssh_auth_keys_skip" ]; then
+	log "Skip check for $ssh_auth_keys"
+elif [ -e "$ssh_auth_keys" ]; then
+	count=$(grep -c ssh-rsa "$ssh_auth_keys")
+	if [ $count -gt 0 ]; then
+		log "Try to use local cache [$ssh_auth_keys] for [$UnsaveUserName] ($$)"
+		exit 0
+	else
+		log "Local cache [$ssh_auth_keys] is emptry for [$UnsaveUserName] ($$)"
+	fi
 fi
 
 # check if AWS CLI exists
@@ -20,6 +43,8 @@ fi
 
 if [[ ! -z "${ASSUMEROLE}" ]]
 then
+  log "assume-role ${ASSUMEROLE}"
+
   STSCredentials=$(aws sts assume-role \
     --role-arn "${ASSUMEROLE}" \
     --role-session-name something \
@@ -33,11 +58,7 @@ then
   export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_SECURITY_TOKEN
 fi
 
-UnsaveUserName="$1"
-UnsaveUserName=${UnsaveUserName//".plus."/"+"}
-UnsaveUserName=${UnsaveUserName//".equal."/"="}
-UnsaveUserName=${UnsaveUserName//".comma."/","}
-UnsaveUserName=${UnsaveUserName//".at."/"@"}
+log "Check iam $UnsaveUserName ($$)"
 
 aws iam list-ssh-public-keys --user-name "$UnsaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read -r KeyId; do
   aws iam get-ssh-public-key --user-name "$UnsaveUserName" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
